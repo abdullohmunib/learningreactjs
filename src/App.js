ded09930e782a9aa33345d7f53316343d4caa74b
19523115 <19523115@students.uii.ac.id>
2022-04-15 17:11:03 +0700

    learning react 01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import Navbar from "./Navbar"; //[02.3] untuk export default
+// import {Navbar} from "./Navbar"; //[02.3] // untuk export tanpa default
+// import logo from './logo.svg';
+// import './App.css';
+
+// cara membuat komponen [01]
+// const Navbar = () => {
+//   return(
+//     <div>
+//       <h3>Navbar</h3>
+//     </div>
+//   )
+// }
+// Cara lain buat folder sendiri [02.1]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Header />
+      <h2>Hello World!</h2>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,32 @@
+// import React from "react"; //[02.2]
+
+// function componen
+// export default function Navbar(){
+//     return(
+//         <div>
+//             <h2>Navbar Juga</h2>
+//         </div>
+//     )
+// }
+
+// atau bisa menggunakan lab componen // ketik rfc
+// import React, { Component } from 'react'
+
+// export default class Navbar extends Component {
+//   render() {
+//     return (
+//       <div>Navbar</div>
+//     )
+//   }
+// }
+
+// atau bisa menggunakan rafc/Arrow Function dan hapus export dibelakang const 
+//  tambahi tilisan paling bawah
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+export default Navbar
